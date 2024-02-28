7e101bbfa187c7a0ea28b4d42b312e4137712896
danberger51 <dan.berger@ict-campus.net>
2024-02-28 14:40:16 +0100

    Erster Commit

---
+++ src/main/java/org/example/fortnite/FortniteApplication.java
@@ -0,0 +1,13 @@
+package org.example.fortnite;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FortniteApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(FortniteApplication.class, args);
+    }
+
+}
